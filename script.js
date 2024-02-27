529c9cafb6f91811f1970efa413c37ff152971f8
Frannerz <francesstephens@hotmail.com>
2024-02-27 20:56:53 +0000

    show/hide + other features added

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const addButton = document.getElementById('add-button');
 const listContainer = document.getElementById('list-container');
-const showComplete = document.getElementById('toggle-complete');
+const showCompleteItems = document.getElementById('show-complete');
+const hideCompleteItems = document.getElementById('hide-complete');
 
 //list to hold the items
 let toDoListItems = [];
 let completedItems = [];
 
+
 //function to add item to list from prompt
 function addItem() {
   const userInput = prompt('Add an item to list');
   const itemId = new Date().getTime();
-  toDoListItems.push({ task: userInput, id: itemId, complete: false, img:'<i class="ph ph-check-circle"></i>' });
+  const task = { task: userInput, id: itemId, complete: false, img:'<i class="ph ph-check-circle"></i>' }
+  
+  if(toDoListItems.length>=10){
+    alert("Your list is full for today! Complete or delete some items so you don't get overwhelmed❤️")
+  } else {
+    toDoListItems.push(task);
+    displayIncomplete(toDoListItems);
+  }
   console.log(toDoListItems);
-  //this triggers the displayItems function
-  displayItems(toDoListItems);
+  
 }
 
 addButton.addEventListener('click', addItem);
 
-//add event listeners to the list container and decide what 
-//to do based on what the event.target is
+
 listContainer.addEventListener('click', function(event) {
     const target = event.target;
     if (target.closest('.complete-button i')) {
@@ -28,15 +35,13 @@ listContainer.addEventListener('click', function(event) {
       deleteItem(event);
     }
   });
-  
+
 function displayItems(array) {
   //empty out list before adding more items
   listContainer.innerHTML = '';
 
   //for each item in the array the is complete:false, display them in list
   array.forEach((task) => {
-    
-    if (!task.complete) {
       const item = document.createElement('li');
       item.id = task.id;
 
@@ -54,26 +59,39 @@ function displayItems(array) {
         `;
       //append each <li> item to the <ul>
       listContainer.append(item);
-
-    }
   });
 }
+
+function displayIncomplete(array){
+  let newArray = array.filter((item)=>!item.complete)
+  displayItems(newArray)
+}
+
+function showComplete(){
+  displayItems(toDoListItems);
+}
+
+function hideComplete(){
+  displayIncomplete(toDoListItems);
+}
+
+showCompleteItems.addEventListener('click', showComplete)
+hideCompleteItems.addEventListener('click', hideComplete)
 
 function completeItem(event) {
   const listItem = event.target.closest('li');
   const completedItem = toDoListItems.find((item)=>item.id === Number(listItem.id));
   
   //mark item as true and change the tick icon
-  completedItem.complete = true;
   completedItem.img = '<i class="ph-fill ph-check-circle"></i>';
+  completedItem.complete = true;
 
   //add this item to the completed items
   completedItems.push(completedItem);
-  
   console.log("complete:", completedItems);
 
   //triggers the display function
-  displayItems(toDoListItems);
+  displayIncomplete(toDoListItems)
 }
 
 function deleteItem(event) {
@@ -86,9 +104,29 @@ function deleteItem(event) {
   console.log("updated list after", toDoListItems);
 
   //triggers the display function
-  displayItems(toDoListItems);
+  displayIncomplete(toDoListItems);
 }
 
-// function showComplete(){
+const regularItems = document.getElementById('regular-items')
 
-// }
+regularItems.addEventListener('click', function(event) {
+  const item = event.target;
+  if (item.tagName === 'BUTTON') { 
+    const task = item.innerText;
+    const itemId = new Date().getTime(); 
+    const taskToAdd = { 
+      task: task, 
+      id: itemId, 
+      complete: false,
+      img: '<i class="ph ph-check-circle"></i>' 
+    };
+    if(toDoListItems.length>=10){
+      alert("Your list is full for today! Complete or delete some items so you don't get overwhelmed ❤️")
+    } else {
+    toDoListItems.push(taskToAdd); 
+    displayIncomplete(toDoListItems); 
+    }
+  }
+});
+
+
