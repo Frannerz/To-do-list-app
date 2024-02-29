842869e0366f0e6540b40c3874444d5d307f2555
Frannerz <francesstephens@hotmail.com>
2024-02-29 18:35:52 +0000

    updates

--- script.js
+++ script.js
@@ -2,39 +2,57 @@ const addButton = document.getElementById('add-button');
 const listContainer = document.getElementById('list-container');
 const showCompleteItems = document.getElementById('show-complete');
 const hideCompleteItems = document.getElementById('hide-complete');
+const targetButton = document.getElementById('target-button');
+const targetDisplay = document.getElementById('target-display');
 
 //list to hold the items
 let toDoListItems = [];
 let completedItems = [];
+let target = 0;
 
+//Set a target for the day
+function setTarget(){
+target = prompt("Type a number to set your target for the day!")
+target = parseInt(target)
+if(!Number.isInteger(target)){
+    target = prompt("Please type a valid integer");
+  }
+  targetDisplay.innerText = target
+}
+targetButton.addEventListener('click', setTarget)
 
 //function to add item to list from prompt
 function addItem() {
   const userInput = prompt('Add an item to list');
   const itemId = new Date().getTime();
-  const task = { task: userInput, id: itemId, complete: false, img:'<i class="ph ph-check-circle"></i>' }
-  
-  if(toDoListItems.length>=10){
-    alert("Your list is full for today! Complete or delete some items so you don't get overwhelmed❤️")
+  const task = {
+    task: userInput,
+    id: itemId,
+    complete: false,
+    img: '<i class="ph ph-check-circle"></i>',
+  };
+
+  if (toDoListItems.length >= 10) {
+    alert(
+      "Your list is full for today! Complete or delete some items so you don't get overwhelmed❤️"
+    );
   } else {
     toDoListItems.push(task);
     displayIncomplete(toDoListItems);
   }
   console.log(toDoListItems);
-  
 }
 
 addButton.addEventListener('click', addItem);
 
-
-listContainer.addEventListener('click', function(event) {
-    const target = event.target;
-    if (target.closest('.complete-button i')) {
-      completeItem(event);
-    } else if (target.closest('.delete-button i')) {
-      deleteItem(event);
-    }
-  });
+listContainer.addEventListener('click', function (event) {
+  const target = event.target;
+  if (target.closest('.complete-button i')) {
+    completeItem(event);
+  } else if (target.closest('.delete-button i')) {
+    deleteItem(event);
+  }
+});
 
 function displayItems(array) {
   //empty out list before adding more items
@@ -42,11 +60,11 @@ function displayItems(array) {
 
   //for each item in the array the is complete:false, display them in list
   array.forEach((task) => {
-      const item = document.createElement('li');
-      item.id = task.id;
+    const item = document.createElement('li');
+    item.id = task.id;
 
-      // each list item will have a complete button, the task and delete button
-      item.innerHTML = `
+    // each list item will have a complete button, the task and delete button
+    item.innerHTML = `
         <div class='complete-button'>
             ${task.img}
         </div>
@@ -57,76 +75,84 @@ function displayItems(array) {
             <i class="ph ph-trash"></i>
         </div>
         `;
-      //append each <li> item to the <ul>
-      listContainer.append(item);
+    //append each <li> item to the <ul>
+    listContainer.append(item);
   });
 }
 
-function displayIncomplete(array){
-  let newArray = array.filter((item)=>!item.complete)
-  displayItems(newArray)
+function displayIncomplete(array) {
+  let newArray = array.filter((item) => !item.complete);
+  displayItems(newArray);
 }
 
-function showComplete(){
+function showComplete() {
   displayItems(toDoListItems);
 }
 
-function hideComplete(){
+function hideComplete() {
   displayIncomplete(toDoListItems);
 }
 
-showCompleteItems.addEventListener('click', showComplete)
-hideCompleteItems.addEventListener('click', hideComplete)
+showCompleteItems.addEventListener('click', showComplete);
+hideCompleteItems.addEventListener('click', hideComplete);
 
 function completeItem(event) {
   const listItem = event.target.closest('li');
-  const completedItem = toDoListItems.find((item)=>item.id === Number(listItem.id));
-  
+  const completedItem = toDoListItems.find(
+    (item) => item.id === Number(listItem.id)
+  );
+
   //mark item as true and change the tick icon
   completedItem.img = '<i class="ph-fill ph-check-circle"></i>';
   completedItem.complete = true;
 
   //add this item to the completed items
   completedItems.push(completedItem);
-  console.log("complete:", completedItems);
-
-  //triggers the display function
-  displayIncomplete(toDoListItems)
-}
-
-function deleteItem(event) {
-  console.log("list before", toDoListItems);
-  const listItem = event.target.closest('li');
-  const index = toDoListItems.findIndex((item)=>item.id === Number(listItem.id));
+  if(completedItems.length===target){
+    alert(`Well done! You have completed ${target} items 🥳`);
+  }
   
-  toDoListItems = toDoListItems.slice(0,index).concat(toDoListItems.slice(index+1));
-  
-  console.log("updated list after", toDoListItems);
+  console.log('complete:', completedItems);
 
   //triggers the display function
   displayIncomplete(toDoListItems);
 }
 
-const regularItems = document.getElementById('regular-items')
+function deleteItem(event) {
+  console.log('list before', toDoListItems);
+  const listItem = event.target.closest('li');
+  const index = toDoListItems.findIndex(
+    (item) => item.id === Number(listItem.id)
+  );
 
-regularItems.addEventListener('click', function(event) {
+  toDoListItems = toDoListItems
+    .slice(0, index)
+    .concat(toDoListItems.slice(index + 1));
+
+  console.log('updated list after', toDoListItems);
+
+  //triggers the display function
+  displayIncomplete(toDoListItems);
+}
+
+const regularItems = document.getElementById('regular-items');
+
+regularItems.addEventListener('click', function (event) {
   const item = event.target;
-  if (item.tagName === 'BUTTON') { 
-    const task = item.innerText;
-    const itemId = new Date().getTime(); 
-    const taskToAdd = { 
-      task: task, 
-      id: itemId, 
-      complete: false,
-      img: '<i class="ph ph-check-circle"></i>' 
-    };
-    if(toDoListItems.length>=10){
-      alert("Your list is full for today! Complete or delete some items so you don't get overwhelmed ❤️")
-    } else {
-    toDoListItems.push(taskToAdd); 
-    displayIncomplete(toDoListItems); 
-    }
+  const task = item.innerText;
+  const itemId = new Date().getTime();
+  const taskToAdd = {
+    task: task,
+    id: itemId,
+    complete: false,
+    img: '<i class="ph ph-check-circle"></i>',
+  };
+  if (toDoListItems.length >= 10) {
+    alert(
+      "Your list is full for today! Complete or delete some items so you don't get overwhelmed ❤️"
+    );
+  } else {
+    toDoListItems.unshift(taskToAdd);
+    displayIncomplete(toDoListItems);
   }
 });
-
-
